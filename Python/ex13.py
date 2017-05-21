adf28e76fb511adfd1a831b8de1b912eceea144c
newshinely <newshine61@126.com>
2017-05-22 00:54:53 +0800

    20170522

---
+++ Python/ex13.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, first, second, third, four = argv
+
+prm1 = raw_input("Please input a prm!")
+prm2 = raw_input("Please input a prm again !")
+
+print "The script is called:", script
+print "Your first variable is :", first
+print "Your second variable is :", second
+print "Your third variable is :", third
+print "Your four variable is :", four
+print "Your input first parameter is :", prm1
+print "Your input second parameter is :", prm2
+
